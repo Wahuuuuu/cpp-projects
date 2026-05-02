0ef8810b744f90be64eb2a5722deac47efe94aee
Wahuuuuu <2080559692@qq.com>
2026-05-02 21:22:11 +0000

    Youxiandeshengming Wujindezhemo

--- Position.h
+++ Position.h
@@ -1,19 +1,23 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include <vector>
+using namespace std;
+
 template <class Key, class Value>
-class Position {
+class Position 
+{
 public: 
 	Position(const Key key); 
 		// 给出一个 key，新建一个 position
 	Position(const Position<Key, Value>& orig); 
 		// 给出一个 position，新建一个它的拷贝 position
 	virtual ~Position(); 
+
 	/* Modificadors */ 
 	// setParent
 	// setLeft
 	// setRight
-	
 	
 	/* Consultors */ 
 	const Key& getKey() const; 
@@ -32,9 +36,49 @@ public:
 	void addValue(const Value& value);  // 往 vector 中新增 value
 	bool operator==(const Position<Key, Value>& other) const;
 
-private: Key key; 
+private: 
+    Key key; 
 	vector<Value> values; 
 	// Afegiu-hi aquí els atributs que manquen
 };
+
+
+
+// Constructors i Destructor
+// -----------------------------
+
+template <class Key, class Value>
+Position<Key, Value>::Position(const Key key) {
+    this->key = key;
+}
+
+
+template <class Key, class Value>
+Position<Key, Value>::Position(const Position<Key, Value>& orig) {
+    this->key = orig.getKey();
+    this->value = orig.getValue();
+}
+
+
+template <class Key, class Value>
+Position<Key, Value>::~Position();
+
+
+// Modificadors
+// ----------------
+
+// setParent
+// setLeft
+// setRight
+
+
+// Consultors
+// --------------
+
+
+
+/* Operacions */
+
+
 
 #endif // POSITION_H
